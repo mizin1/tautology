c2c26877b0c5b90ca7ac58480fc45839786fc63a
mizin1 <konrad_1916@wp.pl>
2014-04-10 22:50:26 +0200

    skrypty do uruchamiania przykladow + run.bat do uruchamiania pod windowsem

---
+++ example1.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp target/tautology-0.0.1-SNAPSHOT.jar pl.waw.mizinski.li.tautology.main.Main '(((A&B)=>(~A+C))+((A&B)=>(~A+C)))=>(((A&B)=>(~A+C))+((A&B)=>(~A&A)))'
---
+++ run.bat
@@ -0,0 +1,2 @@
+java -cp target/tautology-0.0.1-SNAPSHOT.jar pl.waw.mizinski.li.tautology.main.Main %1
+
